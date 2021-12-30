9b3bdf22b67026a39330bec6f4c8559e7e49979b
cristhian <cristhian233027@gmail.com>
2021-12-30 06:05:43 +0100

    first scraping

--- WebDjango/Comparador/views.py
+++ WebDjango/Comparador/views.py
@@ -1,3 +1,27 @@
+import csv
+import requests
 from django.shortcuts import render
 
-# Create your views here.
+
+
+def home(request):
+
+    return render(request, '../templates/home.html',
+                {"": ""})
+
+
+def index(request):
+
+    #leer archivo csv
+    products = list()
+    nombre_archivo = "Comparador/static/img/amazon.csv"
+    with open(nombre_archivo, encoding='utf-8', mode='r') as archivo:
+        lector = csv.reader(archivo, delimiter=",")
+        next(lector, None)
+        for fila in lector:
+            products.append(fila)
+
+    return render(request, '../templates/index.html',
+                  {"products": products})
+
+
